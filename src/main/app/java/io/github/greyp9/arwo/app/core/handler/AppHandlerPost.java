054e1a5c86f5a199f02391e39af42f75f7bd7ff5
greyp9 <greyp9@yahoo.com>
2017-10-07 20:02:54 +0000

    process environment view

--- src/main/app/java/io/github/greyp9/arwo/app/core/handler/AppHandlerPost.java
+++ src/main/app/java/io/github/greyp9/arwo/app/core/handler/AppHandlerPost.java
@@ -1,4 +1,68 @@
 package io.github.greyp9.arwo.app.core.handler;
 
+import io.github.greyp9.arwo.app.core.state.AppUserState;
+import io.github.greyp9.arwo.core.alert.Alert;
+import io.github.greyp9.arwo.core.app.App;
+import io.github.greyp9.arwo.core.app.AppRequest;
+import io.github.greyp9.arwo.core.http.HttpArguments;
+import io.github.greyp9.arwo.core.http.HttpResponse;
+import io.github.greyp9.arwo.core.http.HttpResponseU;
+import io.github.greyp9.arwo.core.http.servlet.ServletHttpRequest;
+import io.github.greyp9.arwo.core.io.StreamU;
+import io.github.greyp9.arwo.core.submit.SubmitToken;
+import io.github.greyp9.arwo.core.submit.SubmitTokenU;
+import io.github.greyp9.arwo.core.value.NameTypeValue;
+import io.github.greyp9.arwo.core.value.NameTypeValues;
+
+import java.io.IOException;
+
 public class AppHandlerPost {
+    private final AppRequest request;
+    private final ServletHttpRequest httpRequest;
+    private final AppUserState userState;
+
+    public AppHandlerPost(final ServletHttpRequest httpRequest, final AppUserState userState) {
+        this.request = userState.getAppRequest(httpRequest);
+        this.httpRequest = httpRequest;
+        this.userState = userState;
+    }
+
+    public final HttpResponse doPostSafe() throws IOException {
+        HttpResponse httpResponse;
+        try {
+            httpResponse = doPost();
+        } catch (IOException e) {
+            userState.getAlerts().add(new Alert(Alert.Severity.ERR, e.getMessage()));
+            httpResponse = HttpResponseU.to500(e.getMessage());
+        }
+        return httpResponse;
+    }
+
+    private HttpResponse doPost() throws IOException {
+        // redirect location (identity by default)
+        String location = httpRequest.getHttpRequest().getResource();
+        final byte[] entity = StreamU.read(httpRequest.getHttpRequest().getEntity());
+        final NameTypeValues httpArguments = HttpArguments.toArguments(entity);
+        for (final NameTypeValue httpArgument : httpArguments) {
+            if (userState.getSubmitID().equals(httpArgument.getName())) {
+                final SubmitToken token = SubmitTokenU.fromString(httpArgument.getValueS());
+                if (token != null) {
+                    location = applySubmit(token, httpArguments, location);
+                }
+            }
+        }
+        return HttpResponseU.to302(location);
+    }
+
+    private String applySubmit(
+            final SubmitToken token, final NameTypeValues httpArguments, final String locationIn) throws IOException {
+        String location = locationIn;
+        final String subject = token.getSubject();
+        if (App.Target.USER_STATE.equals(subject)) {
+            location = userState.applyPost(token, httpArguments, httpRequest);
+        } else if (App.Target.VIEW_STATE.equals(subject)) {
+            userState.getViewStates().apply(token, httpArguments, request.getBundle(), userState.getAlerts());
+        }
+        return location;
+    }
 }
