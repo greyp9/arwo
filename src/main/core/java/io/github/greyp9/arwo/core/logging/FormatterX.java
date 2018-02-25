e0da9367f1f5435b459fd8fd78904f4e027b4fce
greyp9 <greyp9@yahoo.com>
2018-02-25 02:53:43 +0000

    custom formatter; handle exception

--- src/main/core/java/io/github/greyp9/arwo/core/logging/FormatterX.java
+++ src/main/core/java/io/github/greyp9/arwo/core/logging/FormatterX.java
@@ -2,6 +2,8 @@ package io.github.greyp9.arwo.core.logging;
 
 import io.github.greyp9.arwo.core.date.DateX;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.Date;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
@@ -19,12 +21,19 @@ public class FormatterX extends Formatter {
     }
 
     public final String format(final LogRecord record) {
+        final Throwable thrown = record.getThrown();
         return String.format("%s [%d-%s] %-5s %s.%s - %s%n",
                 dateX.toString(new Date(record.getMillis())),
                 record.getThreadID(), Thread.currentThread().getName(),  // NOPMD
                 record.getLevel().toString(),  // NOPMD
                 record.getSourceClassName(),
                 record.getSourceMethodName(),
-                record.getMessage());
+                ((thrown == null) ? record.getMessage() : formatThrowable(thrown)));
+    }
+
+    private String formatThrowable(final Throwable thrown) {
+        final StringWriter writer = new StringWriter();
+        thrown.printStackTrace(new PrintWriter(writer));
+        return writer.toString();
     }
 }
