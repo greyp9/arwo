1c06a1ca0ab4e725ea4d86c54af9b688c0a6c511
greyp9 <greyp9@yahoo.com>
2021-12-31 16:30:51 -0500

    Java 11 compatibility

--- src/main/core/java/io/github/greyp9/arwo/core/codec/b64/Base64Codec.java
+++ src/main/core/java/io/github/greyp9/arwo/core/codec/b64/Base64Codec.java
@@ -1,6 +1,5 @@
 package io.github.greyp9.arwo.core.codec.b64;
 
-import javax.xml.bind.DatatypeConverter;
 import java.io.IOException;
 
 public final class Base64Codec {
@@ -9,10 +8,10 @@ public final class Base64Codec {
     }
 
     public static String encode(final byte[] bytes) {
-        return ((bytes == null) ? null : DatatypeConverter.printBase64Binary(bytes));
+        return ((bytes == null) ? null : java.util.Base64.getEncoder().encodeToString(bytes));
     }
 
     public static byte[] decode(final String string) throws IOException {
-        return ((string == null) ? null : DatatypeConverter.parseBase64Binary(string));
+        return ((string == null) ? null : java.util.Base64.getDecoder().decode(string));
     }
 }
