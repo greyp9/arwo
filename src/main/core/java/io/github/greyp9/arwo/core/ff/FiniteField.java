b98134fa326442ab19f021dcab0988e4a39df242
greyp9 <greyp9@yahoo.com>
2022-02-05 23:02:55 -0500

    finite field

---
+++ src/main/core/java/io/github/greyp9/arwo/core/ff/FiniteField.java
@@ -0,0 +1,39 @@
+package io.github.greyp9.arwo.core.ff;
+
+import io.github.greyp9.arwo.core.codec.hex.HexCodec;
+
+public final class FiniteField {
+
+    private FiniteField() {
+    }
+
+    // GF256 exponent vector
+    public static final byte[] EXP = HexCodec.decode(""
+            + "0103050f113355ff1a2e7296a1f813355fe13848d87395a4f702060a1e2266aa"
+            + "e5345ce43759eb266abed97090abe63153f5040c143c44cc4fd168b8d36eb2cd"
+            + "4cd467a9e03b4dd762a6f10818287888839eb9d06bbddc7f8198b3ce49db769a"
+            + "b5c457f9103050f00b1d2769bbd661a3fe192b7d8792adec2f7193aee92060a0"
+            + "fb163a4ed26db7c25de73256fa153f41c35ee23d47c940c05bed2c749cbfda75"
+            + "9fbad564acef2a7e829dbcdf7a8e89809bb6c158e82365afea256fb1c843c554"
+            + "fc1f2163a5f407091b2d7799b0cb46ca45cf4ade798b8691a8e33e42c651f30e"
+            + "12365aee297b8d8c8f8a8594a7f20d17394bdd7c8497a2fd1c246cb4c752f601"
+            + "03050f113355ff1a2e7296a1f813355fe13848d87395a4f702060a1e2266aae5"
+            + "345ce43759eb266abed97090abe63153f5040c143c44cc4fd168b8d36eb2cd4c"
+            + "d467a9e03b4dd762a6f10818287888839eb9d06bbddc7f8198b3ce49db769ab5"
+            + "c457f9103050f00b1d2769bbd661a3fe192b7d8792adec2f7193aee92060a0fb"
+            + "163a4ed26db7c25de73256fa153f41c35ee23d47c940c05bed2c749cbfda759f"
+            + "bad564acef2a7e829dbcdf7a8e89809bb6c158e82365afea256fb1c843c554fc"
+            + "1f2163a5f407091b2d7799b0cb46ca45cf4ade798b8691a8e33e42c651f30e12"
+            + "365aee297b8d8c8f8a8594a7f20d17394bdd7c8497a2fd1c246cb4c752f60000");
+
+    // GF256 log vector
+    public static final byte[] LOG = HexCodec.decode(""
+            + "00ff190132021ac64bc71b6833eedf036404e00e348d81ef4c7108c8f8691cc1"
+            + "7dc21db5f9b9276a4de4a6729ac90978652f8a05210fe12412f082453593da8e"
+            + "968fdbbd36d0ce94135cd2f14046833866ddfd30bf068b62b325e29822889110"
+            + "7e6e48c3a3b61e423a6b2854fa853dba2b790a159b9f5eca4ed4ace5f373a757"
+            + "af58a850f4ead6744faee9d5e7e6ade82cd7757aeb160bf559cb5fb09ca951a0"
+            + "7f0cf66f17c449ecd8431f2da4767bb7ccbb3e5afb60b1863b52a16caa55299d"
+            + "97b2879061bedcfcbc95cfcd373f5bd15339843c41a26d47142a9e5d56f2d3ab"
+            + "441192d923202e89b47cb8267799e3a5674aeddec531fe180d638c80c0f77007");
+}
---
+++ src/test/core/java/io/github/greyp9/arwo/core/ff/test/FiniteFieldGeneratorTest.java
@@ -0,0 +1,83 @@
+package io.github.greyp9.arwo.core.ff.test;
+
+import io.github.greyp9.arwo.core.codec.hex.HexCodec;
+import io.github.greyp9.arwo.core.ff.FiniteField;
+import io.github.greyp9.arwo.core.hash.secure.HashU;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.logging.Logger;
+
+public class FiniteFieldGeneratorTest {
+    private final Logger logger = Logger.getLogger(getClass().getName());
+
+    @Test
+    public void testLookupTables() {
+        Assert.assertEquals(
+                "07ec21ce3f045f5d9a2121cf58798c94510122ad81272411a50e2918382ac428",
+                HexCodec.encode(HashU.sha256(FiniteField.EXP)));
+        Assert.assertEquals(
+                "f1930ba3ffbadd49b15e5720a34f4eee56d5ad634dfd5aee683f999bc0e2b970",
+                HexCodec.encode(HashU.sha256(FiniteField.LOG)));
+    }
+
+    /**
+     * Calculate GF(256).
+     */
+    @Test
+    public void testGenerateLookupTables() {
+        // https://en.wikiversity.org/wiki/Reed%E2%80%93Solomon_codes_for_coders#Finite_field_arithmetic
+        final int sizeFiniteFieldLog = FINITE_FIELD_SIZE;
+        final int sizeFiniteFieldExponent = sizeFiniteFieldLog * 2;
+
+        final byte[] bytesFiniteFieldExponent = new byte[sizeFiniteFieldExponent];
+        bytesFiniteFieldExponent[0] = 1;
+        for (int i = 1; (i < (sizeFiniteFieldExponent - 2)); ++i) {
+            bytesFiniteFieldExponent[i] = multiply(bytesFiniteFieldExponent[i - 1], AES_GENERATOR);
+        }
+        logger.info(HexCodec.encode(bytesFiniteFieldExponent));
+        Assert.assertEquals(
+                "07ec21ce3f045f5d9a2121cf58798c94510122ad81272411a50e2918382ac428",
+                HexCodec.encode(HashU.sha256(bytesFiniteFieldExponent)));
+
+        final byte[] bytesFiniteFieldLog = new byte[sizeFiniteFieldLog];
+        for (int i = 0; (i < sizeFiniteFieldLog); ++i) {
+            bytesFiniteFieldLog[bytesFiniteFieldExponent[i] & LOW_BYTE_MASK] = (byte) i;
+        }
+        logger.info(HexCodec.encode(bytesFiniteFieldLog));
+        Assert.assertEquals(
+                "f1930ba3ffbadd49b15e5720a34f4eee56d5ad634dfd5aee683f999bc0e2b970",
+                HexCodec.encode(HashU.sha256(bytesFiniteFieldLog)));
+    }
+
+    /**
+     * Adapted from C language example <a href="https://en.wikipedia.org/wiki/Finite_field_arithmetic">here.</a>
+     */
+    private byte multiply(final int a, final int b) {
+        int aa = a;
+        int bb = b;
+        int p = 0;
+        while ((aa != 0) && (bb != 0)) {
+            if ((bb & 0x01) != 0) {  // if isOdd
+                p ^= aa;
+            }
+            if ((aa & HIGH_BIT) != 0) {  // if highBit
+                aa = (aa << 1) ^ AES_FIELD_POLYNOMIAL;
+            } else {
+                aa <<= 1;
+            }
+            bb >>= 1;
+        }
+        return toByte(p);
+    }
+
+    private static final int FINITE_FIELD_SIZE = 256; // Math.pow(2, 8)
+    private static final int AES_GENERATOR = 0x03;
+    private static final int AES_FIELD_POLYNOMIAL = 0x11b;  // x^8 + x^4 + x^3 + x + 1 (0001_0001_1011)
+    private static final int LOW_BYTE_MASK = (1 << Byte.SIZE) - 1;
+    private static final int HIGH_BIT = 0x80;
+
+    private static byte toByte(final int i) {
+        return (byte) (i & LOW_BYTE_MASK);
+    }
+}
