c45487b3b4505a2fa1168dbe95fc0c618a680fc3
greyp9 <greyp9@yahoo.com>
2022-03-18 18:32:20 -0400

    fix broken test

--- src/test/core/java/io/github/greyp9/arwo/core/date/test/DurationSinceTest.java
+++ src/test/core/java/io/github/greyp9/arwo/core/date/test/DurationSinceTest.java
@@ -4,11 +4,19 @@ import io.github.greyp9.arwo.core.date.DateU;
 import io.github.greyp9.arwo.core.date.DurationU;
 import io.github.greyp9.arwo.core.date.XsdDateU;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Date;
+import java.util.logging.Logger;
 
 public class DurationSinceTest {
+    private final Logger logger = Logger.getLogger(getClass().getName());
+
+    @Before
+    public void setUp() throws Exception {
+        logger.finest("setUp()");
+    }
 
     @Test
     public void testDurationSinceOneDay() {
@@ -16,48 +24,50 @@ public class DurationSinceTest {
         final long elapsed = DurationU.Const.ONE_DAY_MILLIS;
         final Date dateBefore = new Date(date.getTime() - elapsed);
         final String duration = DurationU.duration(dateBefore, date);
-        //Assert.assertEquals("P1D", duration);
+        Assert.assertEquals("P1D", duration);
     }
 
     @Test
-    public void testDurationSinceThirtyDays() throws Exception {
+    public void testDurationSinceTwentyOneDays() {
         final Date date = new Date();
-        final long elapsed = DurationU.Const.ONE_DAY_MILLIS * 28;
+        logger.finest(XsdDateU.toXSDZMillis(date));
+        final long elapsed = DurationU.Const.ONE_DAY_MILLIS * 21;
         final Date dateBefore = new Date(date.getTime() - elapsed);
+        logger.finest(XsdDateU.toXSDZMillis(dateBefore));
         final String duration = DurationU.duration(dateBefore, date);
-        Assert.assertEquals("P28D", duration);
+        Assert.assertEquals("P21D", duration);
     }
 
     @Test
-    public void testDurationSubtractP1Y() throws Exception {
+    public void testDurationSubtractP1Y() {
         final Date date = XsdDateU.fromXSDZ("2000-01-01T00:00:00.000Z");
         final Date dateBefore = DurationU.subtract(date, DateU.Const.TZ_GMT, "P1Y");
         Assert.assertEquals("1999-01-01T00:00:00.000Z", XsdDateU.toXSDZMillis(dateBefore));
     }
 
     @Test
-    public void testDurationSubtractP1M() throws Exception {
+    public void testDurationSubtractP1M() {
         final Date date = XsdDateU.fromXSDZ("2000-01-01T00:00:00.000Z");
         final Date dateBefore = DurationU.subtract(date, DateU.Const.TZ_GMT, "P1M");
         Assert.assertEquals("1999-12-01T00:00:00.000Z", XsdDateU.toXSDZMillis(dateBefore));
     }
 
     @Test
-    public void testDurationSubtractP1D() throws Exception {
+    public void testDurationSubtractP1D() {
         final Date date = XsdDateU.fromXSDZ("2000-01-01T00:00:00.000Z");
         final Date dateBefore = DurationU.subtract(date, DateU.Const.TZ_GMT, "P1D");
         Assert.assertEquals("1999-12-31T00:00:00.000Z", XsdDateU.toXSDZMillis(dateBefore));
     }
 
     @Test
-    public void testDurationSubtractP1Y1M1D() throws Exception {
+    public void testDurationSubtractP1Y1M1D() {
         final Date date = XsdDateU.fromXSDZ("2000-01-01T00:00:00.000Z");
         final Date dateBefore = DurationU.subtract(date, DateU.Const.TZ_GMT, "P1Y1M1D");
         Assert.assertEquals("1998-11-30T00:00:00.000Z", XsdDateU.toXSDZMillis(dateBefore));
     }
 
     @Test
-    public void testDurationSubtractP365D() throws Exception {
+    public void testDurationSubtractP365D() {
         final Date date = XsdDateU.fromXSDZ("2000-01-01T00:00:00.000Z");
         final Date dateBefore = DurationU.subtract(date, DateU.Const.TZ_GMT, "P1Y");
         Assert.assertEquals("1999-01-01T00:00:00.000Z", XsdDateU.toXSDZMillis(dateBefore));
