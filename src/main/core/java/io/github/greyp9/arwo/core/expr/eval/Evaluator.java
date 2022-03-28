72a0330e4b7c15557d776ff79a54289c2f4e6406
greyp9 <greyp9@yahoo.com>
2022-03-27 23:30:03 -0400

    prep for EnvironmentKey

--- src/main/core/java/io/github/greyp9/arwo/core/expr/eval/Evaluator.java
+++ src/main/core/java/io/github/greyp9/arwo/core/expr/eval/Evaluator.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public abstract class Evaluator {
 
-    abstract Node evaluate(List<Node> operands);
+    protected abstract Node evaluate(List<Node> operands);
 
     protected final void validateSize(final int size, final List<Node> nodes) {
         if (size != nodes.size()) {
