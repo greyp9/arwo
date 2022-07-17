4156dbf2d1a4c3d22668abd102b0050b40abd2f9
greyp9 <greyp9@yahoo.com>
2022-07-17 18:01:28 -0400

    log certificate expiry at bind

--- src/main/core/java/io/github/greyp9/arwo/core/tls/manage/TLSKeyManager.java
+++ src/main/core/java/io/github/greyp9/arwo/core/tls/manage/TLSKeyManager.java
@@ -1,11 +1,16 @@
 package io.github.greyp9.arwo.core.tls.manage;
 
 import io.github.greyp9.arwo.core.lang.CharU;
+import io.github.greyp9.arwo.core.value.Value;
 
 import javax.net.ssl.KeyManager;
 import javax.net.ssl.KeyManagerFactory;
 import java.security.GeneralSecurityException;
 import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class TLSKeyManager {
     private final KeyStore keyStore;
@@ -21,6 +26,16 @@ public class TLSKeyManager {
         this.keyStore = keyStore;
         this.passphrase = CharU.copy(passphrase);
         this.algorithm = algorithm;
+    }
+
+    public final X509Certificate getCertificate() throws KeyStoreException {
+        final ArrayList<String> aliases = Collections.list(keyStore.aliases());
+        final String alias = (aliases.isEmpty() ? null : aliases.iterator().next());
+        return (alias == null) ? null : getCertificate(alias);
+    }
+
+    public final X509Certificate getCertificate(final String alias) throws KeyStoreException {
+        return Value.as(keyStore.getCertificate(alias), X509Certificate.class);
     }
 
     @SuppressWarnings("PMD.MethodReturnsInternalArray")
