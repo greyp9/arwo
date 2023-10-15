cf6cfacec953a87a2d409887cc2f2af8a613cd6d
greyp9 <greyp9@yahoo.com>
2023-10-15 18:55:03 -0400

    exception utils

--- src/main/core/java/io/github/greyp9/arwo/core/lang/ExceptionU.java
+++ src/main/core/java/io/github/greyp9/arwo/core/lang/ExceptionU.java
@@ -1,5 +1,6 @@
 package io.github.greyp9.arwo.core.lang;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -19,5 +20,17 @@ public final class ExceptionU {
             throwable = throwable.getCause();
         }
         return messages;
+    }
+
+    public static void assertEquals(final long expected, final long actual) throws IOException {
+        if (expected != actual) {
+            throw new IOException(String.format("failure, assertEquals(%d,%d)", expected, actual));
+        }
+    }
+
+    public static void assertNotNull(final Object actual) throws IOException {
+        if (actual == null) {
+            throw new IOException("failure, assertNotNull()");
+        }
     }
 }
