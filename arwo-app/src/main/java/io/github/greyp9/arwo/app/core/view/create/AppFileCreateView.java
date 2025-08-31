456ad4a9359b3137ce6a25f47eceaec43398f7f0
greyp9 <greyp9@yahoo.com>
2025-08-30 21:42:30 -0400

    deprecate XedActionFileNew

--- arwo-app/src/main/java/io/github/greyp9/arwo/app/core/view/create/AppFileCreateView.java
+++ arwo-app/src/main/java/io/github/greyp9/arwo/app/core/view/create/AppFileCreateView.java
@@ -1,25 +1,14 @@
 package io.github.greyp9.arwo.app.core.view.create;
 
 import io.github.greyp9.arwo.app.core.state.AppUserState;
-import io.github.greyp9.arwo.core.action.ActionButtons;
-import io.github.greyp9.arwo.core.action.ActionFactory;
 import io.github.greyp9.arwo.core.app.App;
-import io.github.greyp9.arwo.core.bundle.Bundle;
 import io.github.greyp9.arwo.core.http.HttpResponse;
 import io.github.greyp9.arwo.core.http.servlet.ServletHttpRequest;
 import io.github.greyp9.arwo.core.util.CollectionU;
-import io.github.greyp9.arwo.core.xed.action.XedActionFileNew;
-import io.github.greyp9.arwo.core.xed.cursor.XedCursor;
-import io.github.greyp9.arwo.core.xed.model.Xed;
-import io.github.greyp9.arwo.core.xed.nav.XedNav;
-import io.github.greyp9.arwo.core.xed.request.XedRequest;
-import io.github.greyp9.arwo.core.xed.view.XedPropertyPageView;
-import io.github.greyp9.arwo.core.xed.view.html.PropertyPageHtmlView;
+import io.github.greyp9.arwo.core.xed.action.XedAction;
 import org.w3c.dom.Element;
 
-import javax.xml.namespace.QName;
 import java.io.IOException;
-import java.util.Collection;
 
 public class AppFileCreateView {
     private final ServletHttpRequest httpRequest;
@@ -31,20 +20,10 @@ public class AppFileCreateView {
     }
 
     public final HttpResponse addContentTo(final Element html) throws IOException {
-        // command input form (prep)
-        final XedActionFileNew action = new XedActionFileNew(userState.getXedFactory());
-        final Xed xedUI = action.getXedUI(userState.getLocus().getLocale());
-        final XedCursor cursor = new XedNav(xedUI).getRoot();
-        // command input form
-        final Bundle bundle = cursor.getXed().getBundle();
-        final QName qname = cursor.getTypeInstance().getQName();
-        final String cursorType = qname.toString();
-        final String submitID = userState.getSubmitID();
-        final ActionFactory factory = new ActionFactory(submitID, bundle, App.Target.SESSION, cursorType, null);
-        final Collection<String> actions = CollectionU.toCollection(App.Action.FILE_CREATE);
-        final ActionButtons buttons = factory.create(qname.getLocalPart(), false, actions);
-        final XedRequest xedRequest = new XedRequest(httpRequest, null, userState.getDocumentState());
-        new PropertyPageHtmlView(new XedPropertyPageView(null, cursor, buttons), xedRequest).addContentTo(html);
+        final XedAction action = new XedAction(
+                App.Actions.QNAME_FILE_NEW, userState.getXedFactory(), userState.getLocale());
+        action.addPropertyPageTo(html, userState.getSubmitID(), httpRequest, userState.getDocumentState(),
+                CollectionU.toCollection(App.Action.FILE_CREATE));
         return null;
     }
 }
--- arwo-core/src/main/java/io/github/greyp9/arwo/core/xed/action/XedActionFileNew.java
+++ arwo-core/src/main/java/io/github/greyp9/arwo/core/xed/action/XedActionFileNew.java
@@ -1,13 +1,7 @@
 package io.github.greyp9.arwo.core.xed.action;
 
-import io.github.greyp9.arwo.core.app.App;
-import io.github.greyp9.arwo.core.xed.model.XedFactory;
-
-import java.io.IOException;
-
-public class XedActionFileNew extends XedAction {
-
-    public XedActionFileNew(final XedFactory factory) throws IOException {
-        super(App.Actions.QNAME_FILE_NEW, factory, null);
-    }
+/**
+ * @deprecated
+ */
+public class XedActionFileNew {
 }
