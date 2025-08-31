1072061fadcb1913af1cd0ef1c94129b4f669b0f
greyp9 <greyp9@yahoo.com>
2025-08-30 22:08:06 -0400

    deprecate XedActionFileEdit

--- arwo-app/src/main/java/io/github/greyp9/arwo/app/core/view/editor/AppFileEditView.java
+++ arwo-app/src/main/java/io/github/greyp9/arwo/app/core/view/editor/AppFileEditView.java
@@ -1,8 +1,6 @@
 package io.github.greyp9.arwo.app.core.view.editor;
 
 import io.github.greyp9.arwo.app.core.state.AppUserState;
-import io.github.greyp9.arwo.core.action.ActionButtons;
-import io.github.greyp9.arwo.core.action.ActionFactory;
 import io.github.greyp9.arwo.core.alert.Alert;
 import io.github.greyp9.arwo.core.app.App;
 import io.github.greyp9.arwo.core.bundle.Bundle;
@@ -17,22 +15,11 @@ import io.github.greyp9.arwo.core.http.HttpResponse;
 import io.github.greyp9.arwo.core.http.servlet.ServletHttpRequest;
 import io.github.greyp9.arwo.core.io.StreamU;
 import io.github.greyp9.arwo.core.util.CollectionU;
-import io.github.greyp9.arwo.core.value.NameTypeValues;
 import io.github.greyp9.arwo.core.value.NameTypeValuesU;
-import io.github.greyp9.arwo.core.xed.action.XedActionFileEdit;
-import io.github.greyp9.arwo.core.xed.cursor.XedCursor;
-import io.github.greyp9.arwo.core.xed.model.Xed;
-import io.github.greyp9.arwo.core.xed.nav.XedNav;
-import io.github.greyp9.arwo.core.xed.op.OpUpdate;
-import io.github.greyp9.arwo.core.xed.request.XedRequest;
-import io.github.greyp9.arwo.core.xed.view.XedPropertyPageView;
-import io.github.greyp9.arwo.core.xed.view.html.PropertyPageHtmlView;
-import io.github.greyp9.arwo.core.xsd.value.ValueInstance;
+import io.github.greyp9.arwo.core.xed.action.XedAction;
 import org.w3c.dom.Element;
 
-import javax.xml.namespace.QName;
 import java.io.IOException;
-import java.util.Collection;
 import java.util.Date;
 
 @SuppressWarnings("PMD.ExcessiveImports")
@@ -49,29 +36,19 @@ public class AppFileEditView {
             final Element html, final MetaFile metaFile, final String charset) throws IOException {
         final byte[] bytes = StreamU.read(metaFile.getBytes());
         final String fileText = UTF8Codec.toString(bytes, charset);
-        // command input form (prep)
-        final XedActionFileEdit action = new XedActionFileEdit(userState.getXedFactory());
-        final Xed xedUI = action.getXedUI(userState.getLocus().getLocale());
-        final XedCursor cursor = new XedNav(xedUI).getRoot();
-        final NameTypeValues ntv = NameTypeValuesU.create("fileEdit.fileEditType.file", fileText);
-        final ValueInstance valueInstanceIn = ValueInstance.create(cursor.getTypeInstance(), ntv);
-        new OpUpdate(null, xedUI).apply(cursor.getElement(), valueInstanceIn);
-        // command input form
-        final Bundle bundle = cursor.getXed().getBundle();
-        final QName qname = cursor.getTypeInstance().getQName();
-        final String cursorType = qname.toString();
-        final String submitID = userState.getSubmitID();
-        final ActionFactory factory = new ActionFactory(submitID, bundle, App.Target.SESSION, cursorType, null);
-        final Collection<String> actions = CollectionU.toCollection(App.Action.FILE_UPDATE);
-        final ActionButtons buttons = factory.create(qname.getLocalPart(), false, actions);
-        final XedRequest xedRequest = new XedRequest(httpRequest, null, userState.getDocumentState());
-        new PropertyPageHtmlView(new XedPropertyPageView(null, cursor, buttons), xedRequest).addContentTo(html);
-        // info alert
+
+        final XedAction action = new XedAction(
+                App.Actions.QNAME_FILE_EDIT, userState.getXedFactory(), userState.getLocale());
+        action.update(NameTypeValuesU.create("fileEdit.fileEditType.file", fileText));
+        action.addPropertyPageTo(html, userState.getSubmitID(), httpRequest, userState.getDocumentState(),
+                CollectionU.toCollection(App.Action.FILE_UPDATE));
+
         final FileMetaData metaData = metaFile.getMetaData();
         final String name = new FileX(metaData.getPath()).getFilename();
         final int length = bytes.length;
         final String lastModified = HttpDateU.toHttpZ(new Date(metaData.getLastModified()));
         final String hash = HexCodec.encode(HashU.md5(bytes));
+        final Bundle bundle = action.getXed().getBundle();
         final String message = bundle.format("AppFileEditView.editor", name, length, lastModified, hash);
         userState.getAlerts().add(new Alert(Alert.Severity.INFO, message));
         return null;
--- arwo-core/src/main/java/io/github/greyp9/arwo/core/xed/action/XedActionFileEdit.java
+++ arwo-core/src/main/java/io/github/greyp9/arwo/core/xed/action/XedActionFileEdit.java
@@ -1,13 +1,7 @@
 package io.github.greyp9.arwo.core.xed.action;
 
-import io.github.greyp9.arwo.core.app.App;
-import io.github.greyp9.arwo.core.xed.model.XedFactory;
-
-import java.io.IOException;
-
-public class XedActionFileEdit extends XedAction {
-
-    public XedActionFileEdit(final XedFactory factory) throws IOException {
-        super(App.Actions.QNAME_FILE_EDIT, factory, null);
-    }
+/**
+ * @deprecated
+ */
+public class XedActionFileEdit {
 }
